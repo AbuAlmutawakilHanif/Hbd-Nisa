45c29e25b79fec9bb01117c07b09cf2b725af64a
AbuAlmutawakilHanif <125716671+AbuAlmutawakilHanif@users.noreply.github.com>
2025-08-02 20:24:51 +0700

    Update hbd.js

--- hbd.js
+++ hbd.js
@@ -19,7 +19,7 @@ function showMessage() {
 // Putar musik
 function playMusic() {
   const music = document.getElementById("bgMusic");
-  music.volume = 0.5; // volume kecil
+  music.volume = 1; // volume kecil
   music.play().catch(() => {
     console.log("Autoplay diblokir, pengguna harus klik tombol dulu.");
   });
@@ -59,5 +59,6 @@ function launchConfetti() {
     }
   })();
 }
+
 
 
